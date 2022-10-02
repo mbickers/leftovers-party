ce8c33fed6055977ae18fa0138c1652a74e37cfa
Max Bickers <mbickers@umich.edu>
2022-10-01 21:52:12 -0400

    Added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍗</text></svg>" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <div className="container font-mono min-h-screen min-w-full">
